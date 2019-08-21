eaefd8bfd3fc8b3476fc30199c06f11f28a214df
Carlos Martinez <carlosjaviermartinezparada@gmail.com>
2019-08-21 09:19:40 -0400

    se cambio el fondo por uno mas elegante a ambas interfaces principales y se agrego un texto de bienvenida con animacion

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@
 window.addEventListener('load', () => {
     document.getElementById('content-all').classList.add('imagenes-cargadas');
 });
+
+window.addEventListener('load', () => {
+    document.getElementById('text-animacion').classList.add('text-animacion-img');
+});
