d444fde29524522fcf09e58c63a30c62dcefc7b9
Carlos Martinez <carlosjaviermartinezparada@gmail.com>
2019-08-20 21:23:03 -0400

    Se mejoro la estetica añadiendo un carrousel en la interfaz principal junto a dos archivos javascript para dar el efecto de transition al cargar la pagina

---
+++ js/carrousel.js
@@ -0,0 +1,4 @@
+
+window.addEventListener('load', () => {
+    document.getElementById('container').classList.add('imagenes-cargadas');
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+
+window.addEventListener('load', () => {
+    document.getElementById('content-all').classList.add('imagenes-cargadas');
+});
