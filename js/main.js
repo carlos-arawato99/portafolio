8d45c9304777119968c196302a7a037a325c24ab
Carlos Martinez <carlosjaviermartinezparada@gmail.com>
2019-10-17 13:35:39 -0400

    diseño de mi portafolio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-
-
 window.addEventListener('load', () => {
-    document.getElementById('content-all').classList.add('imagenes-cargadas');
+    document.getElementById('contenido').classList.add('efectLoad');
 });
 
-window.addEventListener('load', () => {
-    document.getElementById('text-animacion').classList.add('text-animacion-img');
-});
+
+
+let boton = document.getElementById('icon');
+let enlaces = document.getElementById('enlaces');
+let contador = 0;
+
+boton.addEventListener('click',function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+      enlaces.classList.remove('dos');
+      enlaces.className = ('enlaces uno');
+      contador = 0;
+    }
+})
